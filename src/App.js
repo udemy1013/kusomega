a3904ac58c8c4f462c8025c695a9c9dedc1b44c8
ryoyoshikawa <udemy1013@gmail.com>
2022-04-25 17:20:11 +0900

    トップページアニメーションの実装

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Feed } from "./components/Feed";
-import { Rightbar } from "./components/Rightbar";
-import { Sidebar } from "./components/Sidebar";
 import { Box, Stack } from "@mui/material";
 import { Navbar } from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <Box>
+    <Box
+      sx={{
+        backgroundColor: "#FCF9ED",
+        height: "100%",
+        backgroundImage:
+          "url('https://s7.gifyu.com/images/1f48fb6bd0b2258f7.gif')",
+        backgroundSize: "cover",
+      }}
+    >
       <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="center">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
     </Box>
   );
 }
